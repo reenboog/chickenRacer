2b6ae6d226732997afc29a832b052d4fed134a76
reenboog <alex.gievsky@gmail.com>
2012-12-08 11:56:29 +0700

    Initial commit.

---
+++ chickenRacer/chickenRacer/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  chickenRacer
+//
+//  Created by Alex Gievsky on 08.12.12.
+//  Copyright spotGames 2012. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ chickenRacer/chickenRacer/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  chickenRacer
+//
+//  Created by Alex Gievsky on 08.12.12.
+//  Copyright spotGames 2012. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
